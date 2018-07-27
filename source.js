c0adf9195b2bdebf130641c71d23ef77487b0a87
Huthman <huthmanwalesalami@yahoo.ca>
2018-07-27 10:47:53 -0400

    seventh commit

--- source.js
+++ source.js
@@ -1,5 +1,17 @@
-$(".rowTwo").flickity({
+var flkty = new Flickity(".rowTwo", {
 	// options
 	cellAlign: "left",
 	contain: true
+});
+
+// SHOPPING CART COUNTER //
+$(function() {
+	var cartItems = 0;
+	$(".rowTwo").on("click", "button", function(event) {
+		event.preventDefault();
+		cartItems++;
+		$(".cart-items")
+			.text(cartItems)
+			.show();
+	});
 });
